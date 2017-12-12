30222498f464f9f3a2b2e9070fbd949fdb441d2b
sunjinlin <272295408@qq.com>
2017-12-12 19:33:45 +0800

    update

---
+++ frameworks/runtime-src/proj.ios/ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LIRTest
+//
+//  Created by x-men on 2017/12/12.
+//  Copyright © 2017年 lynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ frameworks/runtime-src/proj.ios/ios/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LIRTest
+//
+//  Created by x-men on 2017/12/12.
+//  Copyright © 2017年 lynn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
